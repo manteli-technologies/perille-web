e120f095821ec256a6248368b1d1ff2c4cd10dfa
Matti Nelimarkka <matti.nelimarkka@helsinki.fi>
2011-01-28 23:55:34 +0200

    Adding date parsing

--- js/reittiopas-hsl.js
+++ js/reittiopas-hsl.js
@@ -16,6 +16,17 @@ hsl = {
 
 	},
 
+	_parseTime: function(timeString){
+		// HSL returns dates as yyyymmddhhmm -string
+		var t = timeString;
+		return new Date( t.slice(0,4) , // yyyy
+				 t.slice(4,6) , // mm
+				 t.slice(6,8) , // dd
+				 t.slice(8,10)  , // hh
+				 t.slice(10,12) , // mm
+				00, 00 ); // secs, ms:
+	},
+
 	_removeZeros: function( line ) {
 		while( line[0] == '0' ) {
 			line = line.slice(1);
